fe94a7c276ac1ca045d7171c08b79103be592d69
antoniassilva <antoniasilva@udesa.edu.ar>
2025-04-07 14:50:00 -0300

    ver mas y footer

--- proyecto/src/components/SectionDestacada/Destacada.js
+++ proyecto/src/components/SectionDestacada/Destacada.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, {Component}from 'react'
 import {Link} from 'react-router-dom'
- 
-function Destacada(props) {
-    return (
-    <article className="elementos letraPrincipal">
-    <h3>{props.data.title}</h3>
-    <p>{props.data.text}</p>
-    <Link className="enlace" to={`/detalle/${props.data.id}`}>
-                    <p>{props.data.detalle}</p>
-    </Link>
-    <Link className="enlace" to={`/favoritas`}>
-    <p>{props.data.fav}</p>
+
+class Destacada extends Component{
+  constructor (props){
+    super(props)
+    this.state={
+      data: props.data,
+      mostrarContenido: false
+    }
+
+  }
+
+  ocultar(){
+    this.setState({
+      mostrarContenido: !this.state.mostrarContenido
+    })
+  }
+
+  render(){
+
+    return(
+      <article className="elementos letraPrincipal">
+    <h3>{this.state.data.title}</h3>
+    {
+      this.state.mostrarContenido=== true?
+      <>
+        <p>{this.state.data.text}</p>
+      </>
+      :
+      ''
+
+    }
+
+    <Link className="enlace" to={`/detalle/${this.state.data.id}`}>
+                    <p>{this.state.data.detalle}</p>
     </Link>
 
+    <Link className="enlace" to={`/favoritas`}>
+    <p>{this.state.data.fav}</p>
+    </Link>
+
+    <button onClick= {()=> this.ocultar()}> Ver descripcion  </button>
+    
     </article>
 
     )
+  }
 }
+
 
 export default Destacada
