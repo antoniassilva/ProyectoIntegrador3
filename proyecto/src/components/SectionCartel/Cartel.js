f7e25f30a029e93eae586e4cb33cac2c5d63c148
mariapaznobile <pnobile@udesa.edu.ar>
2025-04-07 11:22:14 -0300

    descripcion cartel

--- proyecto/src/components/SectionCartel/Cartel.js
+++ proyecto/src/components/SectionCartel/Cartel.js
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, {Component}from 'react'
 import {Link} from 'react-router-dom'
 
-function Cartel(props) {
-  return (
-    <article class="elementos letraPrincipal">
-    <h3>{props.data.title}</h3>
-    <p>{props.data.text}</p>
+class Cartel extends Component{
+  constructor (props){
+    super(props)
+    this.state={
+      data: props.data,
+      mostrarContenido: false
+    }
 
-    <Link className="enlace" to={`/detalle/${props.data.id}`}>
-                    <p>{props.data.detalle}</p>
+  }
+
+  ocultar(){
+    this.setState({
+      mostrarContenido: !this.state.mostrarContenido
+    })
+  }
+
+  render(){
+
+    return(
+      <article className="elementos letraPrincipal">
+    <h3>{this.state.data.title}</h3>
+    {
+      this.state.mostrarContenido=== true?
+      <>
+        <p>{this.state.data.text}</p>
+      </>
+      :
+      ''
+
+    }
+
+    <Link className="enlace" to={`/detalle/${this.state.data.id}`}>
+                    <p>{this.state.data.detalle}</p>
     </Link>
 
     <Link className="enlace" to={`/favoritas`}>
-    <p>{props.data.fav}</p>
+    <p>{this.state.data.fav}</p>
     </Link>
+
+    <button onClick= {()=> this.ocultar()}> Ver descripcion  </button>
+    
     </article>
 
-    
-  )
+    )
+  }
 }
-
 export default Cartel
