63c584697a842ec7f0f5aab145b55a3eba29854b
antoniassilva <antoniasilva@udesa.edu.ar>
2025-04-13 16:48:19 -0300

    arreglos favoritas

--- proyecto/src/screens/Favoritas/Favoritas.js
+++ proyecto/src/screens/Favoritas/Favoritas.js
@@ -12,17 +12,23 @@ class Favorita extends Component {
 
   componentDidMount() {
     const storage = localStorage.getItem('favorita');
-
+  
     if (storage !== null) {
       const ids = JSON.parse(storage);
+  
+      if (ids.length === 0) {
+        this.setState({ cargando: false });
+        return;
+      }
+  
       const arrayPeliculas = [];
-
+  
       ids.map(id => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=2aa8547904e3b24a3d305a661689f936`)
           .then(response => response.json())
           .then(data => {
             arrayPeliculas.push(data);
-
+  
             if (arrayPeliculas.length === ids.length) {
               this.setState({
                 peliculas: arrayPeliculas,
